c3b1772cc1431eb1c774eba26bca2db4a83558c5
Indy Vanhaelst <indy@pau.be>
2023-04-11 13:23:12 +0200

    update className

--- src/components/video/video.component.tsx
+++ src/components/video/video.component.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Player } from "video-react";
 
+
+//TODO: add correct video
+
 export const Video = () => {
   return (
     <>
       <div className="bg-dark upper pt" id="video" />
       <div className="wrapper pb-5">
         <Player
-          className="video"
           playsInline
           poster="/images/video.png"
           src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
