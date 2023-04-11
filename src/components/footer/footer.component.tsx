b9f61568ac2e565a0b788eb246da0ad755c2ac9e
Indy Vanhaelst <indy@pau.be>
2023-04-11 10:58:23 +0200

    update footer

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -10,22 +10,22 @@ export const Footer = (): JSX.Element => {
         <div className="wrapper grid gap-4">
           <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
             <div className="self-end">
-              <h3 className="mb-1 font-serif text-xl font-bold text-white">
+              <h3 className="mb-1 font-serif font-bold text-white">
                 NOIND
               </h3>
-              <p className="font-serif text-xl text-white">
+              <p className="font-serif text-white">
                 Lokerenbaan 112
                 <br /> 9240 Zele
               </p>
             </div>
             <div className="self-end">
-              <h3 className="font-serif text-xl text-white">
+              <h3 className="font-serif text-white">
                 <span className="font-bold">Telefoon:</span>{" "}
                 <Link href="tel:052446160">052 44 61 60</Link>
               </h3>
-              <h3 className="font-serif text-xl text-white">
+              <h3 className="font-serif text-white">
                 <span className="font-bold">E-mail:</span>{" "}
-                <Link href="tel:info@noind.be">info@noind.be</Link>
+                <Link href="mailto:info@noind.be">info@noind.be</Link>
               </h3>
             </div>
             <div className="mt-5 hidden md:flex">
@@ -34,7 +34,7 @@ export const Footer = (): JSX.Element => {
                   src="/images/facebook.svg"
                   alt="facebook"
                   className="mr-3"
-                  width={36}
+                  width={24}
                 />
               </Link>
               <Link href="http://www.instagram.com" target="_blank">
@@ -42,16 +42,16 @@ export const Footer = (): JSX.Element => {
                   src="/images/instagram.svg"
                   alt="instagram"
                   className="mr-3"
-                  width={36}
+                  width={24}
                 />
               </Link>{" "}
               <Link href="http://www.linkedin.com" target="_blank">
-                <img src="/images/linkedin.svg" alt="linkedin" width={36} />
+                <img src="/images/linkedin.svg" alt="linkedin" width={24} />
               </Link>
             </div>
           </div>
           <div className="">
-            <h3 className="mb-5 font-serif text-xl font-bold text-white">
+            <h3 className="mb-5 font-serif font-bold text-white">
               Schrijf je in op onze nieuwsbrief
             </h3>
             <input
@@ -69,7 +69,7 @@ export const Footer = (): JSX.Element => {
                 src="/images/facebook.svg"
                 alt="facebook"
                 className="mr-3"
-                width={36}
+                width={24}
               />
             </Link>
             <Link href="http://www.instagram.com" target="_blank">
@@ -77,11 +77,11 @@ export const Footer = (): JSX.Element => {
                 src="/images/instagram.svg"
                 alt="instagram"
                 className="mr-3"
-                width={36}
+                width={24}
               />
             </Link>{" "}
             <Link href="http://www.linkedin.com" target="_blank">
-              <img src="/images/linkedin.svg" alt="linkedin" width={36} />
+              <img src="/images/linkedin.svg" alt="linkedin" width={24} />
             </Link>
           </div>
         </div>
