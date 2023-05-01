14fb8fe55c3db2971784c88fce42923eae74681a
Indy Vanhaelst <indy@pau.be>
2023-05-01 16:00:11 +0200

    update order academy

--- src/components/founders/founders.component.tsx
+++ src/components/founders/founders.component.tsx
@@ -11,19 +11,36 @@ export const Founders = () => {
             Het Academy netwerk zal opgebouwd en geleid worden door
             NOIND-founders Mario Fleurinck en Maud Mertens.
           </p>
-
-          <Founder
-            avatar="/images/mario.jpg"
-            name="Mario Fleurinck"
-            description="Mario Fleurinck werkt sinds 1996 in de spits van fundamentele technologische en maatschappelijke vernieuwing, eerst in de ontwikkeling van asset leaseconcepten, dan als pionier in 3D metaal printen en de bouw van de eerste verticaal geïntegreerde digitale fabriek ter wereld, daarna de integratie van hyper computing en vandaag de ontwikkeling van holografische productie op basis van toegevoegde realiteit, de ultieme combinatie van gedecentraliseerd ontwerp en lokale digitale productie. Micro-ondernemen met hoge maatschappelijke impact, altijd onderbouwd met bovengemiddelde financiële rendementen, dat alles overspant een kwarteeuw van creatief ondernemerschap. "
-          />
+          <div className="hidden md:block">
+            <Founder
+              avatar="/images/mario.jpg"
+              name="Mario Fleurinck"
+              description="Mario Fleurinck werkt sinds 1996 in de spits van fundamentele technologische en maatschappelijke vernieuwing, eerst in de ontwikkeling van asset leaseconcepten, dan als pionier in 3D metaal printen en de bouw van de eerste verticaal geïntegreerde digitale fabriek ter wereld, daarna de integratie van hyper computing en vandaag de ontwikkeling van holografische productie op basis van toegevoegde realiteit, de ultieme combinatie van gedecentraliseerd ontwerp en lokale digitale productie. Micro-ondernemen met hoge maatschappelijke impact, altijd onderbouwd met bovengemiddelde financiële rendementen, dat alles overspant een kwarteeuw van creatief ondernemerschap. "
+            />
+          </div>
+          <div className="block md:hidden">
+            <Founder
+                avatar="/images/moud.jpg"
+                name="Maud Mertens"
+                description="Maud Mertens is een master in Gender- en Diversiteitsstudies en ontwikkelt  transformatieprocessen vanuit een practice-oriented onderzoeksstijl berustend op een filosofisch, antropologisch en ideologisch kader. Bruggen bouwen tussen sociale disciplines en digital manufacturing geeft inzicht in toekomstige implicaties van digitale transities op de globale bevolking. Businesstransities worden kritisch getoetst en gespiegeld aan academische kennis met betrekking tot mensprocessen. Deze kennis draagt rechtsreeks bij aan de beroepspraktijk op microniveau en business development over de sectoren heen. "
+            />
+          </div>
         </div>
 
-        <Founder
-          avatar="/images/moud.jpg"
-          name="Maud Mertens"
-          description="Maud Mertens is een master in Gender- en Diversiteitsstudies en ontwikkelt  transformatieprocessen vanuit een practice-oriented onderzoeksstijl berustend op een filosofisch, antropologisch en ideologisch kader. Bruggen bouwen tussen sociale disciplines en digital manufacturing geeft inzicht in toekomstige implicaties van digitale transities op de globale bevolking. Businesstransities worden kritisch getoetst en gespiegeld aan academische kennis met betrekking tot mensprocessen. Deze kennis draagt rechtsreeks bij aan de beroepspraktijk op microniveau en business development over de sectoren heen. "
-        />
+        <div className="hidden md:block">
+          <Founder
+              avatar="/images/moud.jpg"
+              name="Maud Mertens"
+              description="Maud Mertens is een master in Gender- en Diversiteitsstudies en ontwikkelt  transformatieprocessen vanuit een practice-oriented onderzoeksstijl berustend op een filosofisch, antropologisch en ideologisch kader. Bruggen bouwen tussen sociale disciplines en digital manufacturing geeft inzicht in toekomstige implicaties van digitale transities op de globale bevolking. Businesstransities worden kritisch getoetst en gespiegeld aan academische kennis met betrekking tot mensprocessen. Deze kennis draagt rechtsreeks bij aan de beroepspraktijk op microniveau en business development over de sectoren heen. "
+          />
+        </div>
+        <div className="block md:hidden">
+          <Founder
+              avatar="/images/mario.jpg"
+              name="Mario Fleurinck"
+              description="Mario Fleurinck werkt sinds 1996 in de spits van fundamentele technologische en maatschappelijke vernieuwing, eerst in de ontwikkeling van asset leaseconcepten, dan als pionier in 3D metaal printen en de bouw van de eerste verticaal geïntegreerde digitale fabriek ter wereld, daarna de integratie van hyper computing en vandaag de ontwikkeling van holografische productie op basis van toegevoegde realiteit, de ultieme combinatie van gedecentraliseerd ontwerp en lokale digitale productie. Micro-ondernemen met hoge maatschappelijke impact, altijd onderbouwd met bovengemiddelde financiële rendementen, dat alles overspant een kwarteeuw van creatief ondernemerschap. "
+          />
+        </div>
       </div>
     </section>
   );
