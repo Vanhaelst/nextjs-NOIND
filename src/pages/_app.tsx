a8735926d6fae789732b34eee32cbd9b7c3abb92
Indy Vanhaelst <indy.vanhaelst@hotmail.com>
2023-05-24 10:41:07 +0200

    add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -21,6 +21,16 @@ import "../components/working-with/working-with.styles.css";
 const GTM_ID = "GTM-M2KZZ5M";
 export default function App({ Component, pageProps }: AppProps) {
   return <>
+    <Script id="hotjar" strategy="afterInteractive">
+      {` (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3503931,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-3503931','.js?sv=1');`}
+    </Script>
     <Script id="google-tag-manager" strategy="afterInteractive">
       {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
